a307c5f951d6aa9c7a3cbd8b6b47fa3abde05e64
mahdi abbasi <mahdiabbasi1100@gmail.com>
2020-03-21 14:39:22 +0430

    oop: it is class of product and this modules

---
+++ product_class.py
@@ -0,0 +1,31 @@
+# class of product
+class Product:
+    def __init__(self, name, code, weight, hight, width, price):
+        self.name = name
+        self.code = code
+        self.weight = weight
+        self.hight = hight
+        self.width = width
+        self.real_price = price
+        price_for_customer = price + ((price * 30) / 100)
+        self.price_for_customer = price_for_customer
+
+    # get price from price of one kilo bronze and the weight of product
+    def get_price_weight(self):
+        price_of_bronze = int(input('please give the price of one kilo bronze in now : '))
+        self.real_price = price_of_bronze * self.weight
+        price_for_customer = self.real_price + ((self.real_price * 30) / 100)
+        self.price_for_customer = price_for_customer
+
+    def get_dict_data(self):
+        dict_data = {
+            'name': self.name,
+            'code': self.code,
+            'weight': self.weight,
+            'hight': self.hight,
+            'width': self.width,
+            'price': self.real_price,
+            'price_for_customer': self.price_for_customer
+        }
+        return dict_data
+
